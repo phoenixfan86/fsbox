c30f2a77a59be2109902024024863703a31ad460
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-07-31 23:44:18 +0300

    change footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,13 +7,14 @@ const Footer = () => {
 
   return (
     <footer className="flex items-center justify-between bg-(--bg-2) py-[15px] px-[20px] md:py-[25px] md:px-[30px]">
-      <div className="w-[40%]">
+      <div className="">
         <a href="/" className="group text-3xl uppercase font-bold">
           <h1 className="text-(--primary-color-1) group-hover:text-white transition-colors duration-600 grayscale-50">FS<span className="text-white group-hover:text-(--primary-color-1) transition-colors duration-600">box</span>
           </h1>
         </a>
         <span className="text-xs md:text-sm">Кращі моди для ігор</span>
       </div>
+      <span className="text-xs">2025 Fan</span>
       <div className="">
         <TopNav games={games} />
       </div>
