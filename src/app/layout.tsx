f1f4d518d3a57f49b0c59735aac46b1634d5279c
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-08-01 15:28:24 +0300

    change meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ const oswald = Oswald({
 
 export const metadata: Metadata = {
   title: "FSbox - кращі моди для ігор",
-  description: "скачати моди для Minecraft, скачати моди для Stardew Valley, скачати моди для Terraria та інших ігор. Безплатно, швидко, без реєстрації.",
+  description: "Cкачати моди для Minecraft, скачати моди для Stardew Valley, скачати моди для Terraria та інших ігор. Безплатно, швидко, без реєстрації.",
   icons: {
     icon: "/public/favicon.ico"
   }
@@ -44,7 +44,16 @@ export default function RootLayout({
   return (
     <html lang="uk">
       <head>
+        <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=calendar_month" />
+        <link rel="canonical" href="https://fsbox.pp.ua/" />
+        <link rel="stylesheet" href="https://fsbox.pp.ua/page/2" />
+        <meta property="og:title" content="FSBox pp ua" />
+        <meta property="og:description" content="Cкачати моди для Minecraft, скачати моди для Stardew Valley, скачати моди для Terraria та інших ігор. Безплатно, швидко, без реєстрації." />
+        <meta property="og:url" content="https://fsbox.pp.ua/" />
+        <meta property="og:title" content="FSBox pp ua" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="uk_UA" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${nunito.variable} ${oswald.variable} antialiased`}
---
+++ src/mods/minecraft/cobbled-farms.md
@@ -0,0 +1,16 @@
+---
+title: "Скачати мод Cobbled Farms"
+mod_name: "Cobbled Farms"
+game: "Minecraft"
+tags: ["Minecraft", "Mods", "Forge", "Fabric", "1.7.10", "1.16.5", "1.20.2", "1.21", "1.21.1"]
+author: "cobbledstudios"
+author_link: "https://www.curseforge.com/members/cobbledstudios"
+image_first: "https://gitlab.com/cable-mc/cobblemon-assets/-/raw/master/graphics/cobblemon_golurk_banner.png"
+image_second: "https://media.forgecdn.net/attachments/742/408/cobbled-farms-sign.png"
+download_link: "https://www.curseforge.com/minecraft/worlds/cobbled-farms/files/all?page=1&amp;pageSize=20"
+mod_dependencies: ""
+dependencies_link: ""
+date: "2024-08-1"
+---
+
+Ласкаво просимо до Cobbled Farms - однокористувацького світу, який пояснює особливості Cobblemon як новачкам, так і досвідченим тренерам! Ваше завдання - допомогти місцевим жителям цієї маленької фермерської спільноти підтримувати свій прекрасний будинок, поки вони у від'їзді. На щастя, вони знайшли час, щоб залишити після себе книги, які пояснюють все, що вам потрібно знати, щоб підтримувати ферми Кобблмон, і навіть створити власну фермерську спільноту у вашому власному світі або сервері! Нещодавно команда Cobbled Farms побудувала нове портове містечко, де ви можете дізнатися про риболовлю покемонів, новий покедекс, подвійні та потрійні битви! Обов'язково перевіряйте нові версії навчального світу після кожного великого оновлення, щоб дізнатися про будь-які нові функції, які команда розробників Cobblemon може додати. Щасливого фермерства!
