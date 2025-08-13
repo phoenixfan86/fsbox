273401dbedcf635e099d57229638727286c92f91
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-08-13 14:58:18 +0300

    change description

--- src/app/mods/[...slug]/page.tsx
+++ src/app/mods/[...slug]/page.tsx
@@ -22,7 +22,7 @@ export async function generateMetadata({ params }: { params: SlugParams }): Prom
 
   const title = mod.title;
   const lastVersion = getLastVersion(mod)
-  const description = `${mod.title} — ${stripMarkdown(mod.content).slice(0, 160)}...`
+  const description = `${stripMarkdown(mod.content).slice(0, 160)}...`
 
   return {
     alternates: { canonical },
