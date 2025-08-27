5bfdca93fbd7c7748de8a7bf20c043d952ee9180
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-08-27 23:54:41 +0300

    change next-sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
-const { getSortedModsData } = require('./lib/mods');
-
 module.exports = {
   siteUrl: 'https://fsbox.pp.ua',
   generateRobotsTxt: true,
   sitemapSize: 5000,
-
-  async additionalPaths(config) {
-    const mods = await getSortedModsData();
-
-    const validMods = mods.filter(
-      m => typeof m.slug === 'string' && typeof m.gameSlug === 'string'
-    );
-
-    return validMods.map(mod => ({
-      loc: `/mods/${mod.gameSlug}/${mod.slug}`,
-      lastmod: mod.updated || mod.date || new Date().toISOString().split('T')[0],
-    }));
-  },
 };
