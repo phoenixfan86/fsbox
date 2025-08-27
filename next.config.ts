aaa64c807655e352d1974cf3752d3a777fab9a1e
phoenixfan86 <phoenixfan86@gmail.com>
2025-08-27 11:21:48 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/mods/stardewvalley/:slug*",
+        destination: "/mods/stardew-valley/:slug*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
