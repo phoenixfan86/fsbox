cc0397a8ffdaf0aded224d6849eb2c8d84697dbf
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-09-05 19:41:30 +0300

    change tag

--- src/app/instruction/page.tsx
+++ src/app/instruction/page.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
 const instruction = () => {
   return (
     <section className="md:w-[80%] py-[15px] px-[20px] md:py-[25px] md:px-[30px] shadow">
-      <h2 className="text-xl font-semibold mb-1">Як встановити моди: покрокова інструкція</h2>
+      <h1 className="text-xl font-semibold mb-1">Як встановити моди: покрокова інструкція</h1>
       <p className="text-sm text-(--color-3) mb-4 ">Встановлення модів — це найпростіший спосіб додати улюбленій грі нові можливості, предмети, локації та покращення графіки. У цьому керівництві ми розповімо, як правильно завантажити та встановити моди, щоб вони працювали без помилок.</p>
 
       <h3 className="text-lg font-semibold mb-1">Що таке моди?</h3>
