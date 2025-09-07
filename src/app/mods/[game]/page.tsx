a283ad17b99e1c276ee118c0e65f0c2c10ce048a
phoenixfan86 <phoenixfan86@gmail.com>
2025-09-07 19:10:41 +0300

    Update page.tsx

--- src/app/mods/[game]/page.tsx
+++ src/app/mods/[game]/page.tsx
@@ -31,7 +31,7 @@ export async function generateMetadata({
           url: "./img/preview.png",
           width: 1200,
           height: 630,
-          alt: {title},
+          alt: title,
         }
       ],
       url: canonical,
