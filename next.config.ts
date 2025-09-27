3a95d49fe7a74a98a27e329b5e70a68e8774d3d1
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-09-27 19:06:55 +0300

    ch12

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   async headers(){
     return[
       {
+        source: "/sitemap.xml",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/xml",
+          },
+        ],
+      },
+      {
         source: "/robots.txt",
         headers: [
           {
